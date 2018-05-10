dca1df5d03d6c1396f62707e74816b42f6db2177
Caillou <igorlacroute@gmail.com>
2018-05-10 16:38:30 +0200

    made a method placeElement

--- js/content.js
+++ js/content.js
@@ -1,6 +1,23 @@
+// These are made for the player of tf1
 const player_id = 'zonePlayer'
 const src_attribute = 'data-src'
-const new_container = 'description_container'
+const new_container = 'menu_video_program'
+
+// Place a given element in the new_container
+let placeNewElement = (element) => {
+    // Find a place where we want to put our new player
+    let new_places = document.getElementsByClassName(new_container)
+    if (new_places.length > 0) {
+        let new_place = new_places[0]
+        console.log(`Found new place : ${new_place.innerHTML}`)
+        new_place.innerHTML += element
+    } else {
+        // If we didn't find the new place
+        console.log(`Can't find .${new_container}`)
+    }
+}
+
+// Actual execution comes here
 // Get the player's part with a source url
 let src_holder = document.getElementById(player_id)
 if (src_holder) {
@@ -10,23 +27,16 @@ if (src_holder) {
     let wat_url = raw_wat_url.replace(/^\/\//, 'http://')
     console.log(`Cleaned up WAT url : ${wat_url}`)
 
-    // Find a place where we want to put our new player
-    let new_places = document.getElementsByClassName(new_container)
-    if (new_places.length > 0) {
-        let new_place = new_places[0]
-        console.log(`Found new place : ${new_place.innerHTML}`)
-        new_place.innerHTML += `
-        <div class="no-ads-container">
-            <a href="${wat_url}" target="_blank" class="no-ads-link">
-                <span class="icon-heart-empty iconTF1 no-ads-heart"></span>
-                <span>NO ADS PLEASE</span>
-            </a>
-        </div>`
-    } else {
-        // If we didn't find the new place
-        console.log(`Can't find .${new_container}`)
-    }
+    let new_button = `
+    <div class="no-ads-container">
+        <a href="${wat_url}" target="_blank" class="no-ads-link">
+            <span>No ads, please</span>
+        </a>
+    </div>`
+
+    placeNewElement(new_button)
 } else {
     // If player_id not found
     console.log(`Can't find #${player_id}`)
 }
+
