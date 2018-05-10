c4e1c06ab128ebf06411760c220de330123c5003
Caillou <igorlacroute@gmail.com>
2018-05-10 15:45:20 +0200

    cleanup

--- js/content.js
+++ js/content.js
@@ -1,16 +1,16 @@
+const player_id = 'zonePlayer'
+const src_attribute = 'data-src'
 
-// let wat_url = $('.iframe_player').attr('data-src')
-// let message = "Allez, prend cet url (sans les '//' du début) et va-t-en : " + wat_url
-// let content_video = $('#content_video').insertAfter("<div>"+message+"</div>")
-// let container = $('section.center.columnsContainer.pubMargin').replaceWith("<div>"+message+"</div>")
-// console.log(container)
-// alert(`Allez, prend cet url (sans les '//' du début) et va-t-en : ${wat_url}`)
-
-let player_id = 'zonePlayer'
+// Get the player's part with a source url
 let src_holder = document.getElementById(player_id)
 if (src_holder) {
-    let raw_wat_url = src_holder.getAttribute('data-src')
+    // Get the source url
+    let raw_wat_url = src_holder.getAttribute(src_attribute)
     console.log(`Found WAT src : ${raw_wat_url}`)
+    let wat_url = raw_wat_url.replace(/^\/\//, '')
+    console.log(`Cleaned up WAT url : ${wat_url}`)
+
 } else {
+    // If player_id not found
     console.log(`Can't find #${player_id}`)
 }
